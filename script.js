e12ce9be95346432c008c82887898eec61d20c1e
BHAVANI28122 <108610488+BHAVANI28122@users.noreply.github.com>
2023-04-11 15:49:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this.species = species;
+	}
+	getSpecies()
+	{
+		console.log(this.species);
+	}
+	makeSound(){
+		console.log("makeSound");
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	super(species);
+	bark(){
+		console.log("woof");
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	super(species);
+	purr(){
+		console.log("purr");
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
